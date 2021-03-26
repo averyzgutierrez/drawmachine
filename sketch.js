3a8af6c678e4365746b71210a106524b69549b43
AveryZetaGutierrez <61993901+AveryZetaGutierrez@users.noreply.github.com>
2021-03-26 10:23:53 -0700

    upload1

---
+++ sketch.js
@@ -0,0 +1,33 @@
+let backColor;
+
+
+function setup() { 
+  createCanvas(710, 400);
+  button = createButton('Choose a background Color');
+  button.mousePressed(buttonPressed);
+  button.style ('background-color', '#f5efd5');
+  button.position(300, 450);
+  backColor = color( random(255), random(255), random(255) );
+  
+
+} 
+
+function draw() { 
+  background(backColor);  
+  stroke(0);
+  if (mouseIsPressed === true) {
+  rect(mouseX, mouseY, pmouseX, pmouseY);
+  }
+ 
+    
+
+}
+
+function buttonPressed(){
+backColor = color( random(255), random(255), random(255) );
+}
+
+
+
+
+
